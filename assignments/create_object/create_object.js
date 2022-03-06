7759df41ee833eef58cf95b2d871a125b1c2b37b
abishek118 <abishek181@gmail.com>
2022-03-06 10:25:33 +0530

    sub_create_object

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,14 @@
 function CreateObject(arr) {
     // Write your code here
+    const d ={}
+    
+    for (let i = 0;i<arr.length;i++){
+        if(i%2===0){
+            d[arr[i]] = arr[i+1]
+        }
+        
+    }
+    return d 
 }
 
 module.exports = CreateObject;
